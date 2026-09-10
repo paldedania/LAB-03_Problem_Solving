627331e896598e778407b20e175121659d13a6ce
DivyanshSaharan519 <divyansh.saharan.cg@gmail.com>
2026-09-10 17:34:48 +0530

    5 files done by divyansh saharan

---
+++ Python/For_Loops/pattern05.py
@@ -0,0 +1,10 @@
+n = int(input("Enter n: "))
+
+for i in range(1, n + 1):
+    for j in range(n - i):
+        print(" ", end="")
+
+    for j in range(1, 2 * i):
+        print(j, end="")
+
+    print()
