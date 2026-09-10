cb4a5f13d9eb0ffcddf50c827f1e503b334b5673
lakshaymangwanicg <lakshaymangwani.cg@gmail.com>
2026-09-10 17:43:55 +0530

    5 Problem Done by lakshay

---
+++ Python/For_Loops/find-character.py
@@ -0,0 +1,8 @@
+# Enter a name of thing and count same alphabat in string:
+
+name = "naman"
+count = 0
+for text in name:
+    if text == "n":
+        count = count+1
+print("Total same words: ",count)        
